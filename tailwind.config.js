6a9491731cd040ef5b717cacfc628411b4515cae
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-05 22:27:05 +0100

    implementing tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  content: [],
   theme: {
     extend: {},
   },
