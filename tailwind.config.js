e9ba4e5161c0bf992fea892bda81d880240dcf6a
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-05 21:47:38 +0100

    configuring jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   content: [],
   theme: {
     extend: {},
