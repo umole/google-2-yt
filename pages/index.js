77373ebc40933355fdf660ec0b9028382fb0ce1a
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-04-07 23:42:04 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
 import { ViewGridIcon } from '@heroicons/react/solid'
+import Image from 'next/image'
+//import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+//import { MagnifyingGlassIcon } from '@heroicons/react/outline'
 
 export default function Home() {
   return (
@@ -24,7 +27,7 @@ export default function Home() {
           <p className='link'>Images</p>
 
           {/* Icon */}
-          <ViewGridIcon className='h-6 w-6' />
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full cursor-pointer hover:bg-gray-300' />
 
           {/* Avatar */}
           <Avatar url={'https://images.pexels.com/photos/5792641/pexels-photo-5792641.jpeg?auto=compress&cs=tinysrgb&w=600'} />
@@ -32,6 +35,29 @@ export default function Home() {
       </header>
 
       {/* Body */}
+      <form>
+        <Image 
+          src={'https://cdn.pixabay.com/photo/2015/10/31/12/54/google-1015751__340.png'} 
+          height={100}
+          width={300}
+        />
+        <div>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="currentColor" 
+            className="w-6 h-6 mr-3 text-gray-500"
+          >
+            <path 
+              fillRule="evenodd" 
+              d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" 
+              clipRule="evenodd" 
+            />
+          </svg>
+          <input type='text' className='' />
+
+        </div>
+      </form>
 
       {/* Footer */}
     </>
